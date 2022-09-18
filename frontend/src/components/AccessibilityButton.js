7c9c6ffdf3e1529278b94516bb4d054d03368245
Nevo Cohen <nevocohen111@gmail.com>
2022-09-18 15:14:39 +0300

    "main_app"

--- frontend/src/components/AccessibilityButton.js
+++ frontend/src/components/AccessibilityButton.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Portal from "./Portal";
 import { useEffect } from "react";
 
 export default function AccessibilityButton() {
     const [grayScale, setGrayScale] = React.useState(false);
     const [contrast, setContrast] = React.useState(false);
     const [fontSize, setFontSize] = React.useState(false);
+    const [showModal, setShowModal] = React.useState(false);
 
-    useEffect (() => {
+    useEffect(() => {
         document.body.classList.remove("textBG");
     }, [])
 
@@ -45,10 +47,10 @@ export default function AccessibilityButton() {
             document.body.style.fontSize = '1rem';
             document.querySelectorAll('.menuHeader').forEach((element) => {
                 element.style.fontSize = '1.2rem';
-        });
-        document.getElementsByClassName('navbarChangeSize')[0].style.padding = '0rem';
-        document.getElementsByClassName('navbarChangeSize')[0].style.fontSize = '1rem';
-        document.getElementsByClassName('menuButton')[0].style.fontSize = '1rem';
+            });
+            document.getElementsByClassName('navbarChangeSize')[0].style.padding = '0rem';
+            document.getElementsByClassName('navbarChangeSize')[0].style.fontSize = '1rem';
+            document.getElementsByClassName('menuButton')[0].style.fontSize = '1rem';
         }
     }
 
@@ -57,18 +59,19 @@ export default function AccessibilityButton() {
 
     return (
         <>
-            <div className="ui compact menu positive " style={{ width: '144.5px' ,backgroundColor:"#50C878",position:'relative',top:'-14px' }}>
-                <div className="ui simple dropdown item" style={{ width: '144.5px',display:"inline"}} >
-                    <i className="wheelchair icon" style={{marginLeft:"25px"}}></i>
+            <div className="ui compact menu positive " style={{ width: '144.5px', backgroundColor: "#50C878", position: 'relative', top: '-14px' }}>
+                <div className="ui simple dropdown item" style={{ width: '144.5px', display: "inline" }} >
+                    <i className="wheelchair icon" style={{ marginLeft: "25px" }}></i>
                     <i className="dropdown icon"></i>
                     <div className="menu">
                         <button onClick={() => { toggleBodyColorOnClick() }} className="item">Gray Shade <i className="bitbucket icon"></i></button>
-                        <button onClick={() => { toggleBodyContrastOnClick() }} className="item">Contrasity <i className="adjust icon" style={{position:'relative',left:'5px'}}></i></button>
-                        <button onClick={() => { toggleBodyFontSizeOnClick() }} className="item">Big Font <i className="font icon" style={{position:'relative',left:'20px'}}></i></button>
+                        <button onClick={() => { toggleBodyContrastOnClick() }} className="item">Contrasity <i className="adjust icon" style={{ position: 'relative', left: '5px' }}></i></button>
+                        <button onClick={() => { toggleBodyFontSizeOnClick() }} className="item">Big Font <i className="font icon" style={{ position: 'relative', left: '20px' }}></i></button>
+                        <button onClick={() => setShowModal(true)} className="item"><p style={{ fontSize: '0.8rem', color: '#4169e1', fontWeight: 'bolder' }}>Accessibility statement</p></button>
                     </div>
                 </div>
             </div>
-
+            {showModal && <Portal onClose={() => setShowModal(false)} />}
         </>
 
     );
---
+++ frontend/src/components/Portal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+export default function Portal(props) {
+    return ReactDOM.createPortal (
+       <div className="ui dimmer show modals visible active">
+         <div className="ui raised very padded text container segment">
+         <button onClick={props.onClose} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4"  style={{borderRadius:'3px',position:'relative',left:'54.2%',top:'-3.5rem'}}><i class="close icon" style={{color:'black',position:'relative',left:'2px'}}></i></button>
+            <h2 style={{textDecoration:'underline',color:'#4169e1',fontWeight:'bolder',position:"relative",bottom:'5rem'}}>Accessibility Statement for Pizzarela</h2>
+            <p style={{color:'black',position:'relative',bottom:'2rem',fontSize:'1.3rem'}}>We are committed to ensuring digital accessibility for people with disabilities. We are continually improving the user experience for everyone, and applying the relevant accessibility standards.</p>
+            </div>
+         </div>,
+        document.querySelector("#accessibility-root")
+    )
+}
