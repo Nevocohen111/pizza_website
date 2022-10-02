4b3abfcb6bc3ae6f4a32c0d1852458608b3f75a9
Nevo Cohen <nevocohen111@gmail.com>
2022-10-02 13:56:16 +0300

    "main_app"

--- frontend/src/components/MyOrders.jsx
+++ frontend/src/components/MyOrders.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import AuthContext from "../context/AuthProvider";
-import { useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -11,6 +8,8 @@ export default function MyOrders() {
     const { auth } = useContext(AuthContext);
     const name = auth?.name;
     const navigate = useNavigate();
+    let deleteByIdRef = useRef();
+    const cardRef = useRef();
 
     useEffect(() => {
         if (userData?.length > 0)
@@ -21,7 +20,7 @@ export default function MyOrders() {
     useEffect(() => {
         setUserData(JSON.parse(window.localStorage.getItem('OrdersList')));
         document.body.classList.remove("textBG");
-
+        document.body.style.overflow = "auto";
     }, [])
 
 
@@ -40,14 +39,22 @@ export default function MyOrders() {
         setUserData(response.data);
     }
 
-    const handleDeleteById = async (id) => {
-        const requestOptions = {
-            method: 'DELETE',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ "id": `${id}` })
-        };
-        await fetch('http://localhost:8080/user/delete', requestOptions)
+    const handleDeleteById = (id) => {
+        document.getElementById(id).classList.add("slide-out-right")
+        document.body.style.overflow = "hidden";
+        deleteByIdRef = setTimeout(() => {
+            const requestOptions = {
+                method: 'DELETE',
+                mode: 'cors',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "id": `${id}` })
+            };
+            fetch('http://localhost:8080/user/delete', requestOptions)
+            window.location.reload();
+        }, 500);
+        return () => {
+            clearTimeout(deleteByIdRef.current);
+        }
 
     }
 
@@ -55,36 +62,33 @@ export default function MyOrders() {
     return (
         <>
             {auth?.name !== undefined ?
-               <div className="ui center aligned icon header" style={{position:'relative',top:'-5rem',left:'-0.2rem'}} >
-               <i className="circular envelope open  icon" style={{backgroundColor:'#E97451'}}></i>
-               </div>
-                : null}
-
+                <div className="ui center aligned icon header" style={{ position: 'relative', top: '-5rem', left: '-0.2rem' }} >
+                    <i className="circular envelope open  icon" style={{ backgroundColor: '#E97451' }}></i>
+                </div> : null}
             {userData?.length > 0 ?
                 <div>
                     {userData.map((item, index) => (
-                        <div className="container ml-48 slide-in-left" style={{position:"relative",top:'-5rem'}} key={index}>
-                        <div className="flex flex-col items-center justify-center mt-10 " >
-                            <div className="ui padded text container segment mt-10" style={{ opacity: "0.8",backgroundColor:'#E97451' }}>
-                                <div className="text-center ui header" style={{ fontFamily: "serif" }}>{item.pizzaName}</div>
-                                <img src="https://img.pizza/100/100" alt='Changing random Pizza api image' style={{ borderRadius: '50%' }} className="mb-8 ml-72" />
-                                <div className="ui tiny padded text segment" style={{ backgroundColor: '#F5F5DC' }}>
-                                    <p className="ui header text-center">Name: {item.authName}</p>
-                                    <hr></hr>
-                                    <p className="ui header text-center">Price: {item.size === "Small" ? item.price - 3 : item.size === "Medium" ? item.price : item.size === "Large" ? (item.price * 1) + 3 : item.price}$</p>
-                                    <hr></hr>
-                                    <p className="ui header text-center">Size: {item.size}</p>
-                                    <hr></hr>
-                                    <p className="ui header text-center">Address: {item.address}</p>
-                                    <hr></hr>
-                                    <p className="ui header text-center mb-3">Email: {item.email}</p>
-                                    <hr></hr>
-                                    <p className="ui header text-center mb-3">Phone: {item.phone}</p>
+                        <div className="container ml-48" ref={cardRef} id={item.id} style={{ position: "relative", top: '-5rem' }} key={index}>
+                            <div className="flex flex-col items-center justify-center mt-10"  >
+                                <div className="ui padded text container segment mt-10" style={{ opacity: "0.8", backgroundColor: '#E97451' }}>
+                                    <div className="text-center ui header" style={{ fontFamily: "serif" }}>{item.pizzaName}</div>
+                                    <img src="https://img.pizza/100/100" alt='Changing random Pizza api ' style={{ borderRadius: '50%' }} className="mb-8 ml-72" />
+                                    <div className="ui tiny padded text segment" style={{ backgroundColor: '#F5F5DC' }}>
+                                        <p className="ui header text-center">Name: {item.authName}</p>
+                                        <hr></hr>
+                                        <p className="ui header text-center">Price: {item.size === "Small" ? item.price - 3 : item.size === "Medium" ? item.price : item.size === "Large" ? (item.price * 1) + 3 : item.price}$</p>
+                                        <hr></hr>
+                                        <p className="ui header text-center">Size: {item.size}</p>
+                                        <hr></hr>
+                                        <p className="ui header text-center">Address: {item.address}</p>
+                                        <hr></hr>
+                                        <p className="ui header text-center mb-3">Email: {item.email}</p>
+                                        <hr></hr>
+                                        <p className="ui header text-center mb-3">Phone: {item.phone}</p>
+                                    </div>
+                                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleteById(item.id)}>Abort Order</button>
                                 </div>
-                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleteById(item.id)}>Abort Order</button>
-
                             </div>
-                        </div>
                         </div>
                     ))}
                 </div>
@@ -101,8 +105,6 @@ export default function MyOrders() {
                     </div>
                 </div>
             }
-
-
         </>
     )
 }
